2e911d91c9a9a72635e28c8113bb5caacf14a857
eda013 <poukupenga@gmail.com>
2017-05-26 11:48:42 +1200

    got a nock test to work. hits our api client splendidly

--- test/client/app.test.js
+++ test/client/app.test.js
@@ -1,8 +1,8 @@
-import test from 'ava'
-import React from 'react'
-import { shallow, mount, render } from 'enzyme'
-import * as sinon from 'sinon'
-
-import './setup-dom'
-
-import App from '../../client/components/App'
+// import test from 'ava'
+// import React from 'react'
+// import { shallow, mount, render } from 'enzyme'
+// import * as sinon from 'sinon'
+//
+// import './setup-dom'
+//
+// import App from '../../client/components/App'
