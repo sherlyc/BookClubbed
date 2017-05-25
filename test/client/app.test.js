1b72740aa18adfda54faee6ff1346a8a02927c33
eda013 <poukupenga@gmail.com>
2017-05-26 11:53:44 +1200

    first basic enzyme test passed

--- test/client/app.test.js
+++ test/client/app.test.js
@@ -1,8 +1,15 @@
-// import test from 'ava'
-// import React from 'react'
-// import { shallow, mount, render } from 'enzyme'
-// import * as sinon from 'sinon'
-//
-// import './setup-dom'
-//
-// import App from '../../client/components/App'
+import './setup-dom'
+import test from 'ava'
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import * as sinon from 'sinon'
+
+import App from '../../client/components/App'
+import * as api from '../../client/api'
+
+App.prototype.componentDidMount = () => {}
+
+test('<App />', t => {
+  const wrapper = shallow(<App />)
+  t.is(wrapper.find('h1').text(),'Book Clubbed!')
+})
